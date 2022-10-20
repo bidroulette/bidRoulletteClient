7aaa94ee867ff51a633c8bd6ee058b8b1853d984
Adrian Cosme-Halverson <adrian.cosme5850@gmail.com>
2022-10-20 23:12:43 +0000

    Added login information to state.

--- reactApp/bid-roulette/src/LoginPage.js
+++ reactApp/bid-roulette/src/LoginPage.js
@@ -1,36 +1,41 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import { signup, signin } from './Cognito.js'
 
 
 class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username:'',
+      password:'',
+  }
+};
 
   onSubmit = (event) => {
-    event.preventDefault();
-  
+  event.preventDefault();
+  console.log(this.state.username)
   }
   onChange = (event) => {
     event.preventDefault();
-    console.log(event.target)
-    this.setState({
-      userInfo: {
-        username: event.target.value,
-        password: event.target.password,
-    }})
+    console.log(event.target.value)
+    event.target.id === 'username' ? this.setState({username: event.target.value}):
+    this.setState({password: event.target.value});
   }
 
     render() {
       return (
         <>
-          <Form onSubmit={this.props.onSubmit}>
+          <Form onSubmit={this.onSubmit}>
             <Form.Group controlId='username'>
             <Form.Label>Username</Form.Label>
-            <Form.Control onChange={this.props.onChange}></Form.Control>
+            <Form.Control onChange={this.onChange}></Form.Control>
             </Form.Group>
             
             <Form.Group controlId='password'>
             <Form.Label>Password</Form.Label>
-            <Form.Control onChange={this.props.onChange}></Form.Control>
+            <Form.Control onChange={this.onChange}></Form.Control>
             </Form.Group>
             <Button type='submit'>submit</Button>
             </Form>
